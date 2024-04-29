d19aa76cf9e43d50465bfde109c3144bbfbe0208
AleErbisti <alessiaerbisti2021@olivelliputelli.it>
2024-04-29 09:47:19 +0200

    mofifiicheee

---
+++ endpoints/citta.js
@@ -0,0 +1,123 @@
+function endpoint(app, connpool) {
+
+    app.post("/api/immobili", (req, res) => {
+        var errors = []
+        //controllo dati inseriti
+        if (!req.body.nome) {
+            errors.push("No description specified");
+        }
+        if (!req.body.regione) {
+            errors.push("No description specified");
+        }
+        if (req.body.status === "") {
+            errors.push("No status specified");
+        }
+        
+        if (errors.length) {
+            res.status(400).json({ "error": errors.join(",") });
+            return;
+        }
+        var data = {
+            nome: req.body.nome,
+            regione: req.body.regione,
+        }
+
+        var sql = 'INSERT INTO citta (nome, regione) VALUES (?,?)'
+        var params = [data.nome, data.regione]
+        connpool.query(sql, params, (error, results) => {
+            if (error) {
+                res.status(400).json({ "error": error.message })
+                return;
+            }
+            res.json({
+                "message": "success",
+                "data": data,
+                "id": this.insertID
+            })
+            console.log(results)
+        });
+
+    })
+
+
+
+    app.get("/api/immobili", (req, res, next) => {
+        var sql = "select * from citta"
+        var params = []
+        connpool.query(sql, params, (err, rows) => {
+            if (err) {
+              res.status(400).json({"error":err.message});
+              return;
+            }
+            res.json({
+                "message":"success",
+                "data":rows
+            })
+          });
+    });
+
+
+    app.get("/api/immobili/:id", (req, res) => {
+        var sql = "select * from citta where idcitta = ?"
+        var params = [req.params.id]
+        connpool.query(sql, params, (err, rows) => {
+            if (err) {
+              res.status(400).json({"error":err.message});
+              return;
+            }
+            res.json({
+                "message":"success",
+                "data":rows[0]
+            })
+          });
+    });
+
+
+    app.put("/api/immobili/:id", (req, res) => {
+        var data = {
+            description: req.body.description,
+            status: req.body.status,
+        }
+        connpool.execute(
+            `UPDATE citta set 
+               description = COALESCE(?,description), 
+               status = COALESCE(?,status) 
+               WHERE idcitta = ?`,
+            [data.description, data.status, req.params.id],
+            function (err, result) {
+                if (err){
+                    res.status(400).json({"error": err.message})
+                    return;
+                }
+                console.log(result )
+                res.json({
+                    message: "success",
+                    data: data,
+                    changes: result.affectedRows
+                })
+        });
+    })
+
+
+
+    app.delete("/api/immobili/:id", (req, res) => {
+        connpool.execute(
+            'DELETE FROM citta WHERE idCitta = ?',
+            [req.params.id],
+            function (err, result) {
+                if (err){
+                    res.status(400).json({"error": err.message})
+                    return;
+                }
+                res.json({"message":"deleted", changes: result.affectedRows})
+        });
+    })
+
+
+}
+
+
+
+
+
+module.exports = endpoint;
